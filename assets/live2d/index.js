0c7814addc967c309485323ece0d8410d0602d65
six-floor431 <3127345236@qq.com>
2026-04-06 02:54:34 +0800

    Delete assets directory

--- assets/live2d/index.js
+++
@@ -1,6 +0,0 @@
-// Live2D模块入口
-
-export { getLive2DManager, live2DManager } from './live2d-manager.js';
-
-import { getLive2DManager } from './live2d-manager.js';
-export default getLive2DManager();
--- assets/tts/index.js
+++
@@ -1,5 +0,0 @@
-// TTS模块入口
-
-export { TTSCore, ttsCore } from './tts-core.js';
-export { TTSManager, getTTSManager, ttsManager } from './tts-manager.js';
-export { TTSFilter, ttsFilter } from './tts-filter.js';
